ecb985fc3c572d786b1212b21e97f9425bfe5b4c
Ike <ike.mattice@tiger-sheep.com>
2016-05-26 15:19:37 -0700

    add multiple cells with unique labels

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -14,12 +14,32 @@ class ChecklistViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+            label.text = "Walk the doge"
+        } else if indexPath.row == 1 {
+            label.text = "Brush Teef"
+        } else if indexPath.row == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row == 3 {
+            label.text = "DOTA practice"
+        } else if indexPath.row == 4 {
+            label.text = "Remind self that you should be learning iOS"
+        }
+        
+    return cell
+    }
 
 }
 

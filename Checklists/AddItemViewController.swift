9d4c8c176e5a851005b3e31f5e76c798af034874
Ike <ike.mattice@tiger-sheep.com>
2016-06-07 09:36:43 -0700

    add routing for done button on text field keyboard for the additem view.

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -10,4 +10,21 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
     
+    @IBOutlet weak var textField: UITextField!
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        return nil
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    @IBAction func cancel() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }

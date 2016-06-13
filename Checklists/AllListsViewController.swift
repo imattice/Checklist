56b1c05319b90c67baecdda6adf5e7156e59dedb
Ike Mattice <gogreen37@me.com>
2016-06-13 15:41:56 -0700

    return user to a checklist if one was open when the app was terminated.

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate {
+class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate, UINavigationControllerDelegate {
     var dataModel: DataModel!
     
     func cellForTableView(tableView: UITableView) -> UITableViewCell {
@@ -39,6 +39,12 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         }
         dismissViewControllerAnimated(true, completion: nil) 
     }
+    func navigationController(navigationController: UINavigationController,
+                              willShowViewController viewController: UIViewController, animated: Bool){
+        if viewController === self {
+            NSUserDefaults.standardUserDefaults().setInteger(-1, forKey: "ChecklistIndex")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,14 +55,29 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        navigationController?.delegate = self
+        
+        let index = NSUserDefaults.standardUserDefaults().integerForKey("ChecklistIndex")
+        
+        if index != -1 {
+            let checklist = dataModel.lists[index]
+            performSegueWithIdentifier("ShowChecklist", sender: checklist)
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        
         return dataModel.lists.count
     }
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    override func tableView(tableView: UITableView,
+                            cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 
         let cell = cellForTableView(tableView)
         let checklist = dataModel.lists[indexPath.row]
@@ -65,7 +86,10 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         
         return cell
     }
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
+    override func tableView(tableView: UITableView,
+                            didSelectRowAtIndexPath indexPath: NSIndexPath){
+        NSUserDefaults.standardUserDefaults().setInteger(indexPath.row, forKey: "ChecklistIndex")
+        
         let checklist = dataModel.lists[indexPath.row]
         
         performSegueWithIdentifier("ShowChecklist", sender: checklist)
